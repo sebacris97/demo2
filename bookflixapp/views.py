bb0d82388cc0392bca3ce2c0d63d87abd2859456
sebacris97 <51713904+sebacris97@users.noreply.github.com>
2020-07-08 21:17:49 -0300

    Add files via upload

--- bookflixapp/views.py
+++ bookflixapp/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, redirect
 from bookflixapp.models import Trailer, Libro, Novedad, Capitulo, Perfil, Usuario
 from datetime import timedelta
 from django.utils import timezone
-from django.http import request as rq
-from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
+
 from django.contrib.auth.models import User, AnonymousUser
 from django.contrib.auth import hashers, authenticate
 from django.contrib.auth import login as do_login
 from .forms import RegistrationForm, CreateProfileForm
 from .forms import CustomAuthenticationForm as AuthenticationForm
 from .filters import LibroFilter
+
 from django.contrib.auth.decorators import login_required
 from django.db.models import F
 
@@ -19,34 +19,10 @@ from django.db.models import F
 
 
 def perfil_actual(request):
-    usuario = Usuario.objects.filter(user__email=str(request.user))  # me quedo con el usuario logueado
-    perfil = Perfil.objects.filter(usuario__user__email=str(usuario[0]), selected=True)  # me quedo con el perfil seleccionado
+    usuario=Usuario.objects.filter(user__email=str(request.user))  #me quedo con el usuario logueado
+    perfil=Perfil.objects.filter(usuario__user__email=str(usuario[0]), selected=True) #me quedo con el perfil seleccionado
     return perfil[0]
 
-
-@login_required
-def ver_historial(request):
-    perfil = perfil_actual(request)
-    historial = perfil.historial.all()
-    return render(request, "ver_historial.html", {'historial': historial})
-
-
-def high_to_low(modelo, campo):
-    return modelo.objects.all().order_by('-' + str(campo))
-
-def high_to_low(modelo,campo):
-    return modelo.objects.all().order_by('-'+str(campo))
-
-
-def low_to_high(modelo, campo):
-    return modelo.objects.all().order_by(campo)
-
-
-@login_required
-def ver_libros(request):
-    qs = high_to_low(Libro, 'contador')
-    filtro = LibroFilter(request.GET, queryset=qs)
-    return render(request, "ver_libros.html", {"filter": filtro})
 
 def agregar_favoritos(id_libro,perfil):
     libro = Libro.objects.filter(id=id_libro)
@@ -92,7 +68,9 @@ def ver_libros(request,choice=''):
                                                "favoritos": favoritos})
 
 
-def action(request, pk_libro, pk_capitulo):
+
+
+def action(request,pk_libro,pk_capitulo):
     libro = Libro.objects.filter(id=pk_libro)
     libro.update(contador=F('contador') + 1)
     capitulo = Capitulo.objects.filter(id=pk_capitulo)[0]
@@ -100,35 +78,17 @@ def action(request, pk_libro, pk_capitulo):
     perfil.historial.add(*libro) #lo agrgo a la lista de libros leidos
     return redirect(capitulo.pdf.url)
 
-
-def agregarFavorito(request, pk_libro):
-    libro = get_object_or_404(Libro, id=pk_libro)
-    if libro.usuariosfavorito.filter(id=perfil_actual.id).exist():  # Ver si ya lo tiene en favoritos
-        libro.usuariosfavorito.remove(perfil_actual)  # Si ya esta agregado a favoritos, lo saca de favoritos
-    else:
-        libro.usuariosfavorito.add(perfil_actual)  # Si no lo agrego
-    return redirect(ver_capitulos)
-
-
 @login_required
 def ver_capitulos(request, pk):
     capitulos = Capitulo.objects.filter(libro__id=pk)
     if len(capitulos) > 0:  # parche temporal para los libros que no tienen capitulos
         titulo = capitulos[0].libro
-        perfil = perfil_actual(request)
-        libro = Libro.objects.filter(id=pk)  # me quedo con el libro clickeado
-        perfil.historial.add(*libro)  # lo agrgo a la lista de libros leidos
-        libroobjeto = get_object_or_404(Libro, id=pk)
-        is_favorito = False
-        if libroobjeto.usuariosfavorito.filter(id=perfil_actual.id).exist():
-            is_favorito = True
-        # libro.update(contador=F('contador') + 1)
 
         # el parametro lo recibe de urls. lo que hago es filtrar los capitulos
         # que pertenecen al libro que recibo como parametro
         # (si hiciese objects.all() me estoy quedando con todos los capitulos de todos los libros)
 
-        return render(request, "ver_capitulos.html", {"capitulos": capitulos, "titulo": titulo, "is_favorito": is_favorito})
+        return render(request, "ver_capitulos.html", {"capitulos": capitulos, "titulo": titulo})
     else:
         return redirect('/')  # si no se le subio capitulo te manda a index
 
@@ -136,7 +96,6 @@ def ver_capitulos(request, pk):
 @login_required
 def post_search(request):
     return redirect('/verLibros/?titulo__icontains=' + request.POST['search'])
-
 
 def index(request):
     d = timezone.now() - timedelta(days=7)
@@ -148,14 +107,14 @@ def index(request):
         else:
             perfil = perfil_actual(request)
             nombre_perfil = str(perfil)
-        return render(request, "index.html", {"trailers": trailers, "novedades": novedades, "nombre_perfil": nombre_perfil})
-    return render(request, "index.html", {"trailers": trailers, "novedades": novedades})
-
+        return render(request, "index.html", {"trailers":trailers,"novedades": novedades,"nombre_perfil":nombre_perfil})
+    return render(request, "index.html", {"trailers":trailers,"novedades": novedades})
 
 def register(request):
     # Creamos el formulario de autenticación vacío
     form = RegistrationForm(data=request.POST or None)
     if request.method == "POST":
+        
         # Si el formulario es válido...
         if form.is_valid():
 
@@ -169,7 +128,7 @@ def register(request):
             u = User(username=username, first_name=first_name, last_name=last_name, password=realpassword, email=username)
             u.save()
             user = Usuario(user=u, fecha_de_nacimiento=fecha, tarjeta=tarjeta)
-            # Si el usuario se crea correctamente
+            # Si el usuario se crea correctamente 
             if user is not None:
                 # Hacemos el login manualmente
                 user.save()
@@ -178,6 +137,7 @@ def register(request):
                 do_login(request, u)
                 # Y le redireccionamos a la portada
                 return redirect('/')
+
 
     # Si llegamos al final renderizamos el formulario
     return render(request, "registration/register.html", {'form': form})
@@ -208,6 +168,7 @@ def login(request):
                     return redirect('/')
     # Si llegamos al final renderizamos el formulario
     return render(request, "registration/login.html", {'form': form})
+
 
 
 @login_required
@@ -247,3 +208,6 @@ def selecperfil(request):
         return render(request, 'selec_perfil.html', {"perfiles": perfiles})
     if request.method == "POST":
         return render(request, 'perfil.html')
+
+
+
