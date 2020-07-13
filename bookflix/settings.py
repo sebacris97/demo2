192bd9c7c6ca6ea9c234d45913ab802eaf5c8a13
sebacris97 <51713904+sebacris97@users.noreply.github.com>
2020-07-12 22:44:33 -0300

    Update settings.py

--- bookflix/settings.py
+++ bookflix/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'bookflixapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
         'django_filters',
-	'bookflixapp',
         'django_cleanup',
         'rangefilter',
         'django_admin_listfilter_dropdown',
