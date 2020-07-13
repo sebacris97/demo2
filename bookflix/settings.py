d0fc39f46e01e12d99559dee659d09bccbc8db06
Giuliano De Pian <62069571+argyulo@users.noreply.github.com>
2020-07-13 00:22:57 -0300

    Revert "Merge branch 'master' of https://github.com/sebacris97/demo2"
    This reverts commit 673ee3d86bffc6a105afd42171be54ebfd7c158d, reversing
    changes made to 0a0e2ae7c50db3a0f4ea0e61502632fd2da83292.

--- bookflix/settings.py
+++ bookflix/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	'bookflixapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
         'django_filters',
+	'bookflixapp',
         'django_cleanup',
         'rangefilter',
         'django_admin_listfilter_dropdown',
